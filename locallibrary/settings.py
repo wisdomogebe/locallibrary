d85beaa79f949cfe9e5cb2bc9a8012ca3313e765
Wisdom Ogebe <127043625+wisdomogebe@users.noreply.github.com>
2025-06-14 13:37:29 +0100

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://wisdomlocallibrary.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['wisdomlocallibrary.pythonanywhere.com','127.0.0.1']
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
 # ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
